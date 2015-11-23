dda5268a15ec002599293fe3bed9f455a01f4384
Notsosof1 <cawhitlatch2@gmail.com>
2015-11-23 10:50:25 -0800

    Initial Commit

---
+++ Reminder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Reminder
+//
+//  Created by Cynthia Whitlatch on 11/23/15.
+//  Copyright © 2015 Cynthia Whitlatch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Reminder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Reminder
+//
+//  Created by Cynthia Whitlatch on 11/23/15.
+//  Copyright © 2015 Cynthia Whitlatch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
